d4dd39c571389a1f4d08440e1dfd560cd0211705
Araule <lau7tsu@gmail.com>
2022-11-30 09:53:20 +0100

    petit nettoyage

--- urls/programmes/script_urls.sh
+++ urls/programmes/script_urls.sh
@@ -29,14 +29,13 @@ echo "<html><body>" > $fichier_tableau
 echo "<h2>Tableau $basename :</h2>" >> $fichier_tableau
 echo "<br/>" >> $fichier_tableau
 echo "<table>" >> $fichier_tableau
-echo "<tr><th>ligne</th><th>code</th><th>URL</th><th>encodage</th></tr>" >> $fichier_tableau
+echo "<tr><th>ligne</th><th>code</th><th>URL</th><th>encodage</th><th>occurences</th><th>contexte</th></tr>" >> $fichier_tableau
 
 #maintenant on s'occupe des urls
 lineno=1;
-while read -r URL; #-r : true if file exists and is readable.
-
-do
+while read -r URL; do #-r : true if file exists and is readable.
 	echo -e "\tURL : $URL"; #on découpe chacun des appels / on récupère d'abord le code
+
 	# la façon attendue, sans l'option -w de cURL
 	code=$(curl -ILs $URL | grep -e "^HTTP/" | grep -Eo "[0-9]{3}" | tail -n 1) # on récupère code HTTP
 	# egrep = grep -e ; recherche la ligne qui commence par "^HTTP/" et qui contient le code de retour
@@ -59,15 +58,16 @@ do
 	then
 		echo -e "\tencodage non détecté, on prendra UTF-8 par défaut.";
 		charset="UTF-8";
-		# = cURL ne reconnait pas le charset chinois gbk
+		# s'il n'existe pas, on lui donne la valeur UTF-8
 	else
 		echo -e "\tencodage : $charset";
-		# s'il n'existe pas, on lui donne la valeur UTF-8
+
 	fi
 
 	if [[ $code -eq 200 ]]
 	then
-		dump=$(lynx -dump -nolist -assume_charset=$charset -display_charset=$charset $URL)
+		html=$(curl -Ls $URL) > ./aspitations/$basename-$lineno.html;
+		dump=$(lynx -dump -nolist -assume_charset=$charset -display_charset=$charset $URL) > ./dump-texts/$basename-$lineno.txt
 		# -dump  : récupère le texte privé de l'url (sans les balises html)
 		# -nolist : pour ne pas avoir une liste des urls
 		# -assume_charset = on veut récupérer que des pages en utf-8
@@ -80,15 +80,17 @@ do
 			dump=$(echo $dump | iconv -f $charset -t UTF-8//IGNORE) #texte dump converti d'encodage d'origine à UTF-8
 		fi
 
-		occurences=$(echo $dump | grep -Eo "看书|读书|阅读 | 읽지(도|(않(는|았어도)?)?|못하는) | 읽었(으니|(을(뿐이다)?)?)|다(고)?|던|는(지|데)?|느냐|어요) | 읽는(다(면|고|는)?)?|데(서)?|지를|것보다)| 읽어(야(만|합니다|할|겠다)?)?|주(기(도|가)?)|면|며|보(시)?는|(세)?요|니)|볼|봐야겠다는|나가야겠다고|봤자|달라고|내지|들립니다|준다|라|서|나갈|요)|읽을(수(있는|록)?)|거라는|지|까)|읽으(면(서(도)?)?|라고|려는|니까|시(나요|는)?|세요|십시요)|읽고(자하는|서는|나면|난)|읽(던|습니다|다(가|보면)?|나|더라도|죠|도록|기도|게|든|힐|힌다|힙니다|히지)|읽은(것)?|읽자(는)?|읽혀(서|진)?|독서(하(지|다|는)?)|할|한|했다) " | wc -l)
+		occurences=$(echo $dump | grep -Eo "看书|读书|阅读|읽지(도|(않(는|았어도)?)?|못하는)|읽었(으니|(을(뿐이다)?)?)|다(고)?|던|는(지|데)?|느냐|어요)|
+			읽는(다(면|고|는)?)?|데(서)?|지를|것보다)|읽어(야(만|합니다|할|겠다)?)?|주(기(도|가)?)|면|며|보(시)?는|(세)?요|니)|볼|봐야겠다는|나가야겠다고|봤자|
+			달라고|내지|들립니다|준다|라|서|나갈|요)|읽을(수(있는|록)?)|거라는|지|까)|읽으(면(서(도)?)?|라고|려는|니까|시(나요|는)?|세요|십시요)|읽고(자하는|서는|
+			나면|난)|읽(던|습니다|다(가|보면)?|나|더라도|죠|도록|기도|게|든|힐|힌다|힙니다|히지)|읽은(것)?|읽자(는)?|읽혀(서|진)?|독서(하(지|다|는)?)|할|한|했다" | wc -l)
 		echo -e "\toccurences : $occurences";
 		
 		if [[ ! occurences -ne 0 ]]
 		then
-			charset="gbk"
-			dump=$(lynx -dump -nolist -assume_charset=$charset -display_charset=$charset $URL)
 			charset="GBK"
-			dump=$(echo $dump | iconv -f $charset -t UTF-8//IGNORE) 
+			dump=$(lynx -dump -nolist -assume_charset=ASCII -display_charset=ASCII $URL)
+			dump=$(echo $dump | iconv -f $charset -t UTF-8//IGNORE)
 			occurences=$(echo $dump | grep -Eo "看书|读书|阅读" | wc -l)
 			echo -e "\tnouveau charset $charset"
 			echo -e "\tnouvelles occurences $occurences"
@@ -107,3 +109,5 @@ do
 done < $fichier_urls
 echo "</table>" >> $fichier_tableau
 echo "</body></html>" >> $fichier_tableau
+
+#$(echo $html | grep -Eo "charset=(\w|-)+" | cut -d= -f2)
