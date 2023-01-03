3cddd2b9300a7f1858a69e1a82875260e7d8f923
Araule <lau7tsu@gmail.com>
2023-01-03 19:20:05 +0100

    essai script concordances ça marche pô

--- urls/programmes/concordance.sh
+++ urls/programmes/concordance.sh
@@ -26,25 +26,26 @@ echo "
 <!DOCTYPE html>
 <html lang=\"ko\">
 <head>
-  <meta charset=\"UTF-8\">
-  <title>Concordance</title>
+<meta charset=\"UTF-8\">
+<title>Concordance</title>
 </head>
 <body>
 <table>
 <thead>
-  <tr>
-    <th class=\"has-text-left\">Contexte gauche</th>
-    <th>Cible</th>
-    <th class=\"has-text-right\">Contexte droit</th>
-  </tr>
+<tr>
+<th class=\"has-text-left\">Contexte gauche</th>
+<th>Cible</th>
+<th class=\"has-text-right\">Contexte droit</th>
+</tr>
 </thead>
 <tbody>
 "
 
-grep -E -o "(\w+\W+\w*){0,5}$motif(\w*\W+\w+){0,5}" $fichier_texte | sed -E "s/(.*)($motif)(.*)/<tr><td>\1<\/td><td>\2<\/td><td>\3<\/td><\/tr>/"
+grep -Eo "(\w+\W+){0,5}\b$motif\b(\W+\w+){0,5}" $fichier_texte | sed -E "s/(.*)($motif)(.*)/<tr><td>\1<\/td><td>\2<\/td><td>\3<\/td><\/tr>/g"
 #compter les parenthèses et mettre ce chiffre à la place du \3
 # n'a pas fonctionné car prend un nombre comme un un chiffre
 #exemple \29 va me donner comme résultat "resultat du 2(donc le motif) + le chiffre 9 
+
 
 echo "
 </tbody>
--- urls/programmes/script_urls.sh
+++ urls/programmes/script_urls.sh
@@ -11,20 +11,21 @@
 fichier_urls=$1 # le fichier d'URL en entrée
 fichier_tableau=$2 # le fichier HTML en sortie
 
-#vérifier si nos deux fichiers sont bien là
-if [[ $# -ne 2 ]] # -ne : si $# est différent de 2
+# on vérifie si nos deux fichiers sont bien là
+if [[ $# -ne 2 ]] # si $# est différent de 2
 then
 	echo "Ce programme demande exactement deux arguments."
 	exit # le programme se termine
 fi
 
 regexp="看书|读书|阅读|읽지(도|않(는|았어도)?|못하는)|읽었(으니|을(뿐이다)?|다(고)?|던|는(지|데)?|느냐|어요)|읽는(다(면|고|는)?|데(서)?|지를|것보다)|읽어(야(만|합니다|할|겠다)?|주(기(도|가)?)|면|며|보(시)?는|(세)?요|니|볼|봐야겠다는|나가야겠다고|봤자|달라고|내지|들립니다|준다|라|서|나갈|요)|읽을(수(있는|록)?|거라는|지|까)|읽으(면(서(도)?)?|라고|려는|니까|시(나요|는)?|세요|십시요)|읽고(자하는|서는|나면|난)?|읽(던|습니다|다(가|보면)?|나|더라도|죠|도록|기도|게|든|힐|힌다|힙니다|히지)|읽은(것)?|읽자(는)?|읽혀(서|진)?|독서(하(지|다|는)?|할|한|했다)?"
+# notre expression régulière du verbe  lire" en chinois, corréen et français
 
 echo $fichier_urls;
 
-basename=$(basename -s .txt $fichier_urls) # - s : vrai si le fichier txt a bien un nom 
+basename=$(basename -s .txt $fichier_urls) # -s : vrai si le fichier txt a bien un nom 
 # "basename" command in Linux prints the final component in a file path. 
-# => when you want to extract the file name from the long file path.
+#             ==> when you want to extract the file name from the long file path.
 
 echo "<html><body>" > $fichier_tableau
 echo "<h2>Tableau $basename :</h2>" >> $fichier_tableau
@@ -34,11 +35,8 @@ echo "<tr><th>ligne</th><th>code</th><th>URL</th><th>encodage</th><th>aspiration
 
 #maintenant on s'occupe des urls
 lineno=1;
-while read -r URL;  #-r : true if file exists and is readable.
-do
-	echo -e "\tnuméro de ligne : $lineno"
+while read -r URL; do # -r : true if file exists and is readable
 	echo -e "\tURL : $URL"; #on découpe chacun des appels / on récupère d'abord le code
-
 	# la façon attendue, sans l'option -w de cURL
 	code=$(curl -ILs $URL | grep -e "^HTTP/" | grep -Eo "[0-9]{3}" | tail -n 1) # on récupère code HTTP
 	# egrep = grep -e ; recherche la ligne qui commence par "^HTTP/" et qui contient le code de retour
@@ -58,43 +56,42 @@ do
 	echo -e "\tcode : $code";
 
 	if [[ ! $charset ]]
-		then
+	then
 		echo -e "\tencodage non détecté, on prendra UTF-8 par défaut.";
 		charset="UTF-8";
 		#s'il n'existe pas, on lui donne la valeur UTF-8
-
 	else
 		echo -e "\tencodage : $charset";
-
 	fi
 	
 
 	if [[ $code -eq 200 ]]
 	then
 		dump=$(lynx -dump -nolist -assume_charset=$charset -display_charset=$charset $URL)
-		echo "$dump" > ./dumps-text/$basename-$lineno.txt; #l'ajout de guillemet à résolu le problème du dump/contexte
+		echo "$dump" > ./dumps-text/$basename-$lineno.txt; # l'ajout de guillemet à résolu le problème du dump/contexte
 		curl -Ls $URL > ./aspirations/$basename-$lineno.html;
 		# -dump  : récupère le texte privé de l'url (sans les balises html)
 		# -nolist : pour ne pas avoir une liste des urls
 		# -assume_charset = on veut récupérer que des pages en utf-8
-		# -display_charset=si pas utf-8 alors on remplace
+		# -display_charset = si pas utf-8 alors on remplace
 		
 		if [[ $charset -ne "UTF-8" && -n "$dump" ]]
 		# -ne = not equal si $charset est différent de UTF-8 et
 		# -n "dump" = le dump n'est pas vide / pas forcément utile car existe forcément car on a utilisé lynx juste avant
 		then
-			dump=$(echo $dump | iconv -f $charset -t UTF-8//IGNORE) #texte dump converti d'encodage d'origine à UTF-8
+			dump=$(echo "$dump" | iconv -f $charset -t UTF-8//IGNORE) #texte dump converti d'encodage d'origine à UTF-8
 		fi
 
-		occurences=$(echo $dump | grep -Eo $regexp | wc -l)
+		occurences=$(echo "$dump" | grep -Eo $regexp | wc -l)
 		echo -e "\toccurences : $occurences";
 		
 		if [[ ! occurences -ne 0 ]]
+		# dans le cas des urls chinoises, il arrive que certains sites soient encodés en gbk, 
+		# et lynx ne prend pas en compte cette encodage, alors on utilise w3m
 		then
 			dump=$(w3m $URL)
-			echo "$dump" 
 			echo "$dump" > ./dumps-text/$basename-$lineno.txt;
-			occurences=$(echo $dump | grep -Eo $regexp | wc -l)
+			occurences=$(echo "$dump" | grep -Eo $regexp | wc -l)
 			echo -e "\tnouvelles occurences : $occurences";
 		fi
 
@@ -106,13 +103,13 @@ do
 	fi
 
 
-	egrep -E -B1 -A1 $regexp ./dumps-text/$basename-$lineno.txt > ./contextes/$basename-$lineno.txt
+	egrep -E -B2 -A2 $regexp ./dumps-text/$basename-$lineno.txt > ./contextes/$basename-$lineno.txt
 		
 	bash programmes/concordance.sh ./dumps-text/$basename-$lineno.txt $regexp > ./concordances/$basename-$lineno.html
 	# construction des concordances avec une commande externe
 
 	
-	echo "<tr><td>$lineno</td><td>$code</td><td><a href=\"$URL\">$URL</a></td><td>$charset</td><td>$occurences</td><td><td><a href="./aspirations/$basename-$lineno.txt">$basename-$lineno</a></td><a href="./contextes/$basename-$lineno.txt">$basename-$lineno</a></td><td><a href="./concordances/$basename-$lineno.html">$basename-$lineno</a></tr>" >> $fichier_tableau
+	echo "<tr><td>$lineno</td><td>$code</td><td><a href=\"$URL\">$URL</a></td><td>$charset</td><td><a href="./aspirations/$basename-$lineno.txt">$basename-$lineno</a></td><td>$occurences</td><td><a href="./contextes/$basename-$lineno.txt">$basename-$lineno</a></td><td><a href="./concordances/$basename-$lineno.html">$basename-$lineno</a></tr>" >> $fichier_tableau
 	echo -e "\t--------------------------------"
 	
 	lineno=$((lineno+1));
